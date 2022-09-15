c1de24d4a41ef96533afa85f36493cfbfebaa966
Piyush Sharma <piyush316692@gmail.com>
2022-09-15 18:03:11 +0530

    Made changes to  database uri

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://sgdfkhtjzqvgbb:02672d94316d5eab1847ad4af9344f0cdfc4e9927b02f5db7f69695e151b77e4@ec2-44-207-133-100.compute-1.amazonaws.com:5432/d9eu5dsjona8kl', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
